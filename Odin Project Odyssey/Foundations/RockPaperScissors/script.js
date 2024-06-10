6412650faa6a6743a89f2aec1df8f928dd9e537f
Gwenola <gwenola.leroux@gmail.com>
2024-06-10 14:36:50 -0400

    RockPaperScissors w/ JS : ok

--- Odin Project Odyssey/Foundations/RockPaperScissors/script.js
+++ Odin Project Odyssey/Foundations/RockPaperScissors/script.js
@@ -1,81 +1,110 @@
 // Options
 const Options = ["Rock", "Paper", "Scissors"];
 
+const winConditions = {
+    "Rock": "Scissors",
+    "Paper": "Rock",
+    "Scissors": "Paper"
+};
+
 // Computer choice
 function getComputerChoice() {
     return Options[Math.floor(Math.random() * Options.length)]
 }
 
-// Human choice
-function getHumanChoice() {
-    const choice = prompt("Rock, Paper or Scissors ?").toLowerCase();
-    const capitalized_choice = choice.charAt(0).toUpperCase() + choice.slice(1);
+// Scores
+let human_score = 0;
+let computer_score = 0;
 
-    return Options.includes(capitalized_choice) ? capitalized_choice : "Error - Invalid input";
-}
+// Human choice
+let human_choice = '';
+
+const options = document.querySelector('#options');
+
+options.addEventListener('click', (e) => {
+    let choice = e.target;
+    if ((human_score === 5) || (computer_score === 5)) {
+        resetVariables();
+    }
+    playRound(choice.id);
+});
 
 // Play round
-function playRound(human_choice, computer_choice) {
-    if (human_choice === computer_choice) {
-        console.log("Oh no! It's a tie! Play again :)");
-        return null;
-    }
-
-    const winConditions = {
-        "Rock": "Scissors",
-        "Paper": "Rock",
-        "Scissors": "Paper"
-    };
-
-    if (winConditions[human_choice] === computer_choice) {
-        console.log(`You win! ${human_choice} beats ${computer_choice}.`);
-        return 'human';
-    } else {
-        console.log(`You lose! ${computer_choice} beats ${human_choice}.`);
-        return 'computer';
-    }
-}
-
-// Play game
-function playGame() {
-    let human_rounds = 0;
-    let computer_rounds = 0;
+function playRound(human_selection) {
+    const computer_selection = getComputerChoice();
+    const play = document.querySelector('#play');
+    play.innerHTML = '';
     
-    for (let round = 1; round <= 5; round++) {
-        console.log(`Round: ${round}`);
-        let human_score = 0;
-        let computer_score = 0;
+    const display_hsel = document.createElement('p');
+    display_hsel.textContent = `You've selected ${human_selection}.`;
+    
+    const display_csel = document.createElement('p');
+    display_csel.textContent = `Computer has selected ${computer_selection}.`;
+    
+    play.appendChild(display_hsel);
+    play.appendChild(display_csel);
+
+    const score = document.querySelector('#score');
+    score.innerHTML = '';
+
+    if (human_selection === computer_selection) {
+        const display_tie = document.createElement('p');
+        display_tie.textContent = "Oh no! It's a tie! Play again :)";
+        play.appendChild(display_tie);
+
+        const tie_display = document.createElement('p');
+        tie_display.textContent = `${human_score} - ${computer_score}`;
+        score.appendChild(tie_display);
+    } else if (winConditions[human_selection] === computer_selection) {
+        const winAction = document.createElement('p');
+        winAction.textContent = `You win! ${human_selection} beats ${computer_selection}.`;
+        play.appendChild(winAction);
         
-        while ((human_score < 3) && (computer_score < 3)) {
-            const human_selection = getHumanChoice();
-
-            if (human_selection === "Error - Invalid input") {
-                console.log("Error - Invalid input");
-                continue;
-            }
-
-            console.log(`You've selected ${human_selection}`)
-            const computer_selection = getComputerChoice();
-            console.log(`Computer has selected ${computer_selection}`)
-
-            const winner = playRound(human_selection, computer_selection);
-            if (winner === 'human') {
-                human_score++;
-            } else if (winner === 'computer') {
-                computer_score++;
-            }
-            console.log(`Your score: ${human_score} - Computer's score: ${computer_score}`)
-            
-            if (human_score === 3) {
-                human_rounds++
-                console.log(`You win the round! :) - Score is ${human_rounds} - ${computer_rounds}`)
-            } else if (computer_score === 3) {
-                computer_rounds++
-                console.log(`Oh no! You lose the round :( - Score is ${human_rounds} - ${computer_rounds}`)
-            }
-        }
+        human_score++;
+        const hs_display = document.createElement('p');
+        hs_display.textContent = `${human_score} - ${computer_score}`;
+        score.appendChild(hs_display);
+    } else {
+        const loseAction = document.createElement('p');
+        loseAction.textContent = `You lose! ${computer_selection} beats ${human_selection}.`;
+        play.appendChild(loseAction);
+        
+        computer_score++;
+        const cs_display = document.createElement('p');
+        cs_display.textContent = `${human_score} - ${computer_score}`;
+        score.appendChild(cs_display);
     }
-    console.log(human_rounds > computer_rounds ? "You win the game!!!" : "You lose the game...")
-}
+    
+    if ((human_score == 5) || (computer_score == 5)) {
+        display_win();
+        const btn = document.createElement('button');
+        btn.textContent = 'New game';
+        win.appendChild(btn);
 
-playGame()
+        btn.addEventListener('click', () => {
+            resetVariables();
+        });
+    }
+};
+
+// Display win
+function display_win() {
+    win = document.querySelector('#win');
+
+    const end = document.createElement('p');
+    human_score > computer_score ? end.textContent = "You win the game!!!" : end.textContent = "You lose the game...";
+    win.appendChild(end);
+};
+
+// Reset after a game
+function resetVariables() {
+    human_score = 0;
+    computer_score = 0;
+
+    const score = document.querySelector('#score');
+    score.innerHTML = '';
+    const play = document.querySelector('#play');
+    play.innerHTML = '';
+    const win = document.querySelector('#win');
+    win.innerHTML = '';
+}
