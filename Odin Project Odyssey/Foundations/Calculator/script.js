25fdd31052d745a8bd5f204ed87f6d4b31656e36
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-06-18 07:47:29 -0400

    Calculator : done

--- Odin Project Odyssey/Foundations/Calculator/script.js
+++ Odin Project Odyssey/Foundations/Calculator/script.js
@@ -1,10 +1,26 @@
-// enum
+// global variables
+let operator = '';
+let nb1 = '';
+let nb2 = '';
+let solution = 0;
+let dotState = false;
+
 operation = {
 	'+' : 1,
 	'-' : 2,
 	'×' : 3,
 	'/' : 4
 };
+
+const buttons = document.querySelectorAll('button');
+const numbersBtn = document.querySelectorAll('.numbers');
+const operatorsBtn = document.querySelectorAll('.operators');
+const display = document.querySelector('.display');
+const dotBtn = document.querySelector('.dot');
+const backBtn = document.querySelector('.back');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
+
 
 // basic operations
 function add(nb1, nb2) {
@@ -29,12 +45,6 @@ function divide(nb1, nb2) {
 	return nb1 / nb2;
 }
 
-// global variables
-let operator = '';
-let nb1 = '';
-let nb2 = '';
-let solution = 0;
-let dotState = false;
 
 // operate function
 function operate(operator, nb1, nb2) {
@@ -50,14 +60,13 @@ function operate(operator, nb1, nb2) {
 	}
 };
 
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display');
 
-buttons.forEach((btn) => {
+// Events Listener
+numbersBtn.forEach((btn) => {
 	btn.addEventListener('click', (event) => {
 		const toDisplay = document.createElement('p');
 		toDisplay.className = 'displayed';
-		if (event.target.className === 'numbers' && operator === '') {
+		if (operator === '') {
 			if (nb1 === '0' && event.target.textContent != '.') {
 				display.innerHTML = '';
 				nb1 = event.target.textContent;
@@ -69,8 +78,7 @@ buttons.forEach((btn) => {
 			nb1 += event.target.textContent;
 			toDisplay.textContent = nb1;
 			display.appendChild(toDisplay);
-			console.log(nb1);
-		} else if (event.target.className === 'numbers' && operator != '') {
+		} else {
 			if (nb2 === '0' && event.target.textContent != '.') {
 				display.innerHTML = '';
 				nb2 = event.target.textContent;
@@ -82,8 +90,16 @@ buttons.forEach((btn) => {
 			nb2 += event.target.textContent;
 			toDisplay.textContent = nb2;
 			display.appendChild(toDisplay);
-			console.log(nb2);
-		} else if (event.target.className === 'operators' && nb1 != '') {
+		}
+	});
+});
+
+operatorsBtn.forEach((btn) => {
+	btn.addEventListener('click', (event) => {
+		const toDisplay = document.createElement('p');
+		toDisplay.className = 'displayed';
+
+		if (nb1 != '') {
 			dotState = false;
 			if (nb1 != '' && nb2 != '') {
 				display.innerHTML = '';
@@ -94,61 +110,80 @@ buttons.forEach((btn) => {
 				display.appendChild(toDisplay);
 			}
 			operator = event.target.textContent;
-			console.log(operator);
-		} else if (event.target.className === 'dot' && dotState === false) {
-			display.innerHTML = '';
-			dotState = true;
-			if (operator === '') {
-				if (nb1 === '') {
-					nb1 += 0;
-				}
-				nb1 += event.target.textContent;
-				toDisplay.textContent = nb1;
-			} else {
-				if (nb2 === '') {
-					nb2 += 0;
-				}
-				nb2 += event.target.textContent;
-				toDisplay.textContent = nb2;
-			}
-			display.appendChild(toDisplay);
-		} else if (event.target.className === 'back') {
-			display.innerHTML = '';
-			if (operator === '') {
-				nb1 = nb1.slice(0, nb1.length - 1);
-				console.log(nb1);
-				if (nb1 === '') {
-					nb1 = '0';
-				}
-				toDisplay.textContent = nb1;
-			} else {
-				nb2 = nb2.slice(0, nb2.length - 1);
-				console.log(nb2);
-				if (nb2 === '') {
-					nb2 = '0';
-				}
-				toDisplay.textContent = nb2;
-			}
-			display.appendChild(toDisplay);
-		} else if (event.target.className === 'equal') {
-			if (nb1 != '' && nb2 != '' && operator != '') {
-				display.innerHTML = '';
-				solution = operate(operator, parseFloat(nb1), parseFloat(nb2));
-				toDisplay.textContent = solution;
-				display.appendChild(toDisplay);
-				operator = '';
-				nb1 = '';
-				nb2 = '';
-				dotState = false;
-			}
-		} else if (event.target.className === 'clear') {
-			display.innerHTML = '';
-			operator = '';
-			nb1 = '';
-			nb2 = '';
-			dotState = false;
-			toDisplay.textContent = '0';
-			display.appendChild(toDisplay);
 		}
 	});
+});
+
+dotBtn.addEventListener('click', (event) => {
+	const toDisplay = document.createElement('p');
+	toDisplay.className = 'displayed';
+
+	if (dotState === false) {
+		display.innerHTML = '';
+		dotState = true;
+		if (operator === '') {
+			if (nb1 === '') {
+				nb1 += 0;
+			}
+			nb1 += event.target.textContent;
+			toDisplay.textContent = nb1;
+		} else {
+			if (nb2 === '') {
+				nb2 += 0;
+			}
+			nb2 += event.target.textContent;
+			toDisplay.textContent = nb2;
+		}
+		display.appendChild(toDisplay);
+	}
+});
+
+backBtn.addEventListener('click', () => {
+	const toDisplay = document.createElement('p');
+	toDisplay.className = 'displayed';
+
+	display.innerHTML = '';
+	if (operator === '') {
+		nb1 = nb1.slice(0, nb1.length - 1);
+		if (nb1 === '') {
+			nb1 = '0';
+		}
+		toDisplay.textContent = nb1;
+	} else {
+		nb2 = nb2.slice(0, nb2.length - 1);
+		if (nb2 === '') {
+			nb2 = '0';
+		}
+		toDisplay.textContent = nb2;
+	}
+	display.appendChild(toDisplay);
+});
+
+equalBtn.addEventListener('click', () => {
+	const toDisplay = document.createElement('p');
+	toDisplay.className = 'displayed';
+
+	if (nb1 != '' && nb2 != '' && operator != '') {
+		display.innerHTML = '';
+		solution = operate(operator, parseFloat(nb1), parseFloat(nb2));
+		toDisplay.textContent = Number.isInteger(solution) ? solution : solution.toFixed(2);
+		display.appendChild(toDisplay);
+		operator = '';
+		nb1 = '';
+		nb2 = '';
+		dotState = false;
+	}
+});
+
+clearBtn.addEventListener('click', () => {
+	const toDisplay = document.createElement('p');
+	toDisplay.className = 'displayed';
+
+	display.innerHTML = '';
+	operator = '';
+	nb1 = '';
+	nb2 = '';
+	dotState = false;
+	toDisplay.textContent = '0';
+	display.appendChild(toDisplay);
 });
