3d1eee4fdc56037ddc8dcad9562fc51c2bf36d05
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-07-01 17:49:14 -0400

    TicTacToe: browser game WIP

--- Odin Project Odyssey/FullStack JavaScript/4.TicTacToe/images/script.js
+++ Odin Project Odyssey/FullStack JavaScript/4.TicTacToe/images/script.js
@@ -1,6 +1,6 @@
-function createPlayer(name, marker) {
+function createPlayer(name, marker, color) {
 	const score = 0;
-	return { name, marker, score };
+	return { name, marker, color, score };
 }
 
 // const player1 = createPlayer(prompt('Player One:', 'John'), 'X');
@@ -11,6 +11,12 @@ const gameboard = (function() {
 	const board = Array(9).fill('');
 
 	const getBoard = () => board;
+
+	const resetBoard = () => {
+		for (let i = 0; i < 9; i++) {
+			getBoard[i] = '';
+		}
+	};
 
 	const displayGameboard = () => {
 		let board = []
@@ -25,21 +31,23 @@ const gameboard = (function() {
 			}
 			board.push(line);
 		}
-		console.log(gameboard.getBoard);
+		console.log(gameboard.getBoard());
 	};
 
-	return { getBoard, displayGameboard };
+	return { getBoard, displayGameboard, resetBoard };
 })();
 
 const gameController = (function() {
 	let activePlayer = null;
 	let tickedCase = null;
-	const players = [createPlayer('John', 'X'), createPlayer('Jim', 'O')];
+	const players = [createPlayer('John', 'X', 'blue'), createPlayer('Jim', 'O', 'orange')];
 	const winCon = [
 		[0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal winCon
 		[0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical winCon
 		[0, 4, 8], [2, 4, 6] // diagonal winCon
 	]
+
+	const getActivePlayer = () => activePlayer;
 
 	const switchPlayer = () => {
 		activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
@@ -65,12 +73,17 @@ const gameController = (function() {
 	};
 
 	const tickCase = (id, marker) => {
+		console.log('tickCase:', id, marker);
 		tickedCase = parseInt(id);
 		if (!gameboard.getBoard[id]) {
 			gameboard.getBoard[id] = marker;
+			console.log(gameboard.getBoard[id]);
+			return true;
 		} else {
 			// alert('This case is already marked');
+			console.log(gameboard.getBoard[id]);
 			console.log('This case is already marked');
+			return false;
 		}
 	};
 
@@ -79,27 +92,61 @@ const gameController = (function() {
 
 	}
 
-	function playRound() {
+	function playRound(id, box) {
 		switchPlayer();
 		console.log(activePlayer);
-		console.log(tickedCase);
-		tickCase(tickedCase, activePlayer.marker);
-		gameboard.displayGameboard();
-		checkWinCondition(tickedCase, activePlayer.marker);
-	}
+		if (tickCase(id, activePlayer.marker)) {
+			box.style.backgroundColor = gameController.getActivePlayer().color;
+
+			gameboard.displayGameboard();
+			if (checkWinCondition(tickedCase, activePlayer.marker)) {
+				boxes.classList.add('no-click');
+				return false;
+			}
+		}
+		return true;
+	};
+
+	const endGame = () => {
+		console.log(`The winner is ${activePlayer.name}`);
+		const newGame = document.querySelector('#new');
+		newGame.style.visibility = 'visible';
+	};
 	
-	return { playGame, tickedCase, playRound };
+	return { playGame, tickedCase, playRound, getActivePlayer, endGame };
 })();
 
-const boxes = document.querySelectorAll('.box');
+const body = document.querySelector('body');
+const boxes = document.querySelector('.grid');
+const box = document.querySelectorAll('.box');
+const btnNewG = document.querySelectorAll('button');
 
-boxes.forEach((box) => {
+box.forEach((box) => {
 	box.addEventListener('click', (event) => {
-		console.log('Box:', event.target.id);
-		gameController.tickedCase = event.target.id;
-		console.log(gameController.tickedCase);
-		gameController.playRound();
+		console.log(boxes.classList);
+		if (!(boxes.classList.contains('no-click'))) {
+			console.log('Box:', event.target.id);
+			gameController.tickedCase = event.target.id;
+			console.log(gameController.tickedCase);
+			if (!gameController.playRound(event.target.id, event.target)) {
+				gameController.endGame();
+			}
+		}
 	})
+})
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	document.addEventListener('click', (event) => {
+		if (event.target.id === 'new') {
+			box.forEach((box) => {
+				box.style.backgroundColor = 'white';
+			});
+			event.target.style.visibility = 'hidden';
+			boxes.classList.remove('no-click');
+			gameboard.resetBoard();
+			gameController.playGame();
+		};
+	});
 })
 
 gameController.playGame();
