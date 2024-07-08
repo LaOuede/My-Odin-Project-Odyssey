09a03a6a91c2d4c8a4fdcd46bb6b28790259d20f
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-07-08 17:18:42 -0400

    Restaurant page: Webpack OK, content WIP

---
+++ Odin Project Odyssey/FullStack JavaScript/5.Restaurant Page/src/index.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+console.log('Restaurant Page test 2');
--- Odin Project Odyssey/test.js
+++ Odin Project Odyssey/test.js
@@ -1,13 +1,27 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages`;
+const requestURL =
+  "https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json";
+
+
+fetch(requestURL)
+  .then(response => response.text())
+  .then(text => JSON.parse(text))
+  .then(text => displayCatInfo(text))
+
+function displayCatInfo(obj) {
+  console.log(obj);
+  const header = document.querySelector("header");
+  const myH1 = document.createElement("h1");
+  myH1.textContent = 'My lovely cats';
+  header.appendChild(myH1);
+
+  for (cat in obj) {
+    const myList = document.createElement("h3");
+    myList.textContent = obj[cat].name;
+    header.appendChild(myList);
+    for (kitten in obj[cat].kittens) {
+      const kittensList = document.createElement("p");
+      kittensList.textContent = obj[cat].kittens[kitten].name;
+      header.appendChild(kittensList);
+    }
   }
-};
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-
-console.log(theHobbit.info());
+}
