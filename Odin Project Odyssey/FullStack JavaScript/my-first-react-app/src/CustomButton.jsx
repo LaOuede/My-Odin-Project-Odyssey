df19a7af266a37bbcbab24e4fdd3ecb5d14d37a2
Gwenola Le Roux <114024436+LaOuede@users.noreply.github.com>
2025-02-04 13:06:30 -0500

    Delete Odin Project Odyssey/FullStack JavaScript/my-first-react-app directory

--- Odin Project Odyssey/FullStack JavaScript/my-first-react-app/src/CustomButton.jsx
+++
@@ -1,9 +0,0 @@
-// CustomButton.jsx
-
-const CustomButton = ({ onClick }) => {
-	return (
-	  <button onClick={onClick}>Click me</button> 
-	);
-  };
-  
-  export default CustomButton;
--- Odin Project Odyssey/FullStack JavaScript/my-first-react-app/src/Greeting.jsx
+++
@@ -1,5 +0,0 @@
-function Marauder() {
-	return <h1>&quot;I solemnly swear that I am up to no good&quot;</h1>
-}
-
-export default Marauder;
